97827519d1c86c0ab3699087d666459efde1208e
Ali Baran <119418664+AliBrn@users.noreply.github.com>
2023-03-13 23:51:51 +0300

    Update and rename odev1.java to Finding Min and Max Values ​​from Entered Numbers.java

--- third_part/Finding Min and Max Values ​​from Entered Numbers.java
+++ third_part/Finding Min and Max Values ​​from Entered Numbers.java
@@ -2,7 +2,7 @@ package patikajava.ucuncu_kisim;
 
 import java.util.Scanner;
 
-public class odev1 {
+public class Finding Min and Max Values from Entered Numbers {
        //Kullanıcı girdiği sayılardan en küçüğünü ve en büyüğünü bulan Java Programı
     public static void main(String[] args) {
         Scanner klavye=new Scanner(System.in);
