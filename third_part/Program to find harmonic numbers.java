2ebee392b46f50d0d26ef34a1f72ba564f85129a
Ali Baran <119418664+AliBrn@users.noreply.github.com>
2023-03-13 23:41:17 +0300

    Update and rename pratik9.java to Program to find harmonic numbers.java

--- third_part/Program to find harmonic numbers.java
+++ third_part/Program to find harmonic numbers.java
@@ -2,13 +2,13 @@ package patikajava.ucuncu_kisim;
 
 import java.util.Scanner;
 
-public class pratik9 {
+public class Program to find harmonic numbers {
     //Harmonik serisi 1+1/2+1/3+.... 1/n giden seriyi toplama
     public static void main(String[] args) {
         Scanner klavye = new Scanner(System.in);
         System.out.println("Bir sayı giriniz");
         int n = klavye.nextInt();
-        double total = 0;
+        double total =0;
         for (int i = 1; i <= n; i++) {
             total = total + (1.0 / i);
         }
