666466bbe737cf09b314fcf5c7f8c33b06eaedaf
Ali Baran <119418664+AliBrn@users.noreply.github.com>
2023-03-13 22:51:06 +0300

    Update and rename pratik5.java to Combination.java

--- third_part/Combination.java
+++ third_part/Combination.java
@@ -2,7 +2,7 @@ package patikajava.ucuncu_kisim;
 
 import java.util.Scanner;
 
-public class pratik5 {
+public class Combination {
     //Kombinasyon hesabı yapan Java programı
     public static void main(String[] args) {
         Scanner klavye=new Scanner(System.in);
