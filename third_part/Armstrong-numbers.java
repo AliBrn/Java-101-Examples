6a454d603603ccacdd7338383c8daf9891b09a5d
Ali Baran <119418664+AliBrn@users.noreply.github.com>
2023-03-14 14:36:37 +0300

    Update and rename pratik7.java to Armstrong-numbers.java

--- third_part/Armstrong-numbers.java
+++ third_part/Armstrong-numbers.java
@@ -2,7 +2,7 @@ package patikajava.ucuncu_kisim;
 
 import java.util.Scanner;
 
-public class pratik7 {
+public class Armstrong-numbers {
     // Armstrong sayı olup olmadıgın kontrol eden Java Programı
     public static void main(String[] args) {
         Scanner klavye=new Scanner(System.in);
