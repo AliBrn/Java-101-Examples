7f642b2556a45f2aec1142abf8a6d7caab807929
Ali Baran <119418664+AliBrn@users.noreply.github.com>
2023-03-15 15:07:04 +0300

    Update and rename Sort numbers from smallest to largest.java to Sort_numbers_from_smallest_to_largest.java

--- second_part/Sort_numbers_from_smallest_to_largest.java
+++ second_part/Sort_numbers_from_smallest_to_largest.java
@@ -2,7 +2,7 @@ package patikajava.ikinci_kisim;
 
 import java.util.Scanner;
 
-public class Sort numbers from smallest to largest {
+public class Sort_numbers_from_smallest_to_largest {
     // Girilen 3 sayıyı küçükten büyüğe sıralama
     public static void main(String[] args) {
         Scanner klavye=new Scanner(System.in);
