2083b1ce27ee09d7a8797d0b4fa102fb18cb9b99
Ali Baran <119418664+AliBrn@users.noreply.github.com>
2023-03-15 15:50:23 +0300

    Update and rename even-and-4-multiples-numbers-adder-program.java to Even_and_4_multiples_numbers_adder_program.java

--- third_part/Even_and_4_multiples_numbers_adder_program.java
+++ third_part/Even_and_4_multiples_numbers_adder_program.java
@@ -2,7 +2,7 @@ package patikajava.ucuncu_kisim;
 
 import java.util.Scanner;
 
-public class even and 4 multiples numbers adder program {
+public class Even_and_4_multiples_numbers_adder_program {
     // Girilen sayılardan 4 bölünenleri toplayan tek sayı girdiğinde  Java Programını durduran program.
     public static void main(String[] args) {
         Scanner klavye=new Scanner(System.in);
